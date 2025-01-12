fa3740135473ee74ee721474996779d7f276b2bd
Frodini <151853364+Frodini@users.noreply.github.com>
2025-01-12 01:27:59 +0100

    Add endpoint for get audit-log by admins

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { Service } from "typedi";
 import { UserController } from "../app/user/user.controller";
+import { AuditLogController } from "../app/audit-log/audit-log.controller";
 
 @Service()
 export class Api {
   private apiRouter: Router;
 
-  constructor(private userController: UserController) {
+  constructor(
+    private userController: UserController,
+    private auditLogController: AuditLogController
+  ) {
     this.apiRouter = Router();
     this.initializeRoutes();
   }
@@ -14,6 +18,7 @@ export class Api {
   private initializeRoutes(): void {
     // Rutas de usuario
     this.apiRouter.use("/users", this.userController.router);
+    this.apiRouter.use("/audit-log", this.auditLogController.router); // Rutas de auditoría
 
     // Aquí puedes agregar otros módulos, como:
     // this.apiRouter.use("/appointments", this.appointmentsController.router);
