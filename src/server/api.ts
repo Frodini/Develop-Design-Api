11fda11508e95413e72ccdb658621a23f9168a3c
Frodini <151853364+Frodini@users.noreply.github.com>
2025-01-12 12:05:52 +0100

    Add model, repository, service and controller for availability

--- src/server/api.ts
+++ src/server/api.ts
@@ -3,6 +3,7 @@ import { Service } from "typedi";
 import { UserController } from "../app/user/user.controller";
 import { AuditLogController } from "../app/audit-log/audit-log.controller";
 import { AppointmentController } from "../app/appointment/appoinment.controller";
+import { AvailabilityController } from "../app/availability/availabilty.controller";
 
 @Service()
 export class Api {
@@ -11,7 +12,8 @@ export class Api {
   constructor(
     private userController: UserController,
     private auditLogController: AuditLogController,
-    private appointmentController: AppointmentController
+    private appointmentController: AppointmentController,
+    private availabilityController: AvailabilityController
   ) {
     this.apiRouter = Router();
     this.initializeRoutes();
@@ -22,6 +24,7 @@ export class Api {
     this.apiRouter.use("/users", this.userController.router);
     this.apiRouter.use("/audit-log", this.auditLogController.router); // Rutas de auditoría
     this.apiRouter.use("/appointments", this.appointmentController.router);
+    this.apiRouter.use("/availability", this.availabilityController.router);
 
     // Aquí puedes agregar otros módulos, como:
     // this.apiRouter.use("/appointments", this.appointmentsController.router);
